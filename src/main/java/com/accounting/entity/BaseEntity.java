8168e070a25ba4eb90f412b10d85a6970452fe22
civanozbay <civan417@hotmail.com>
2024-04-15 18:48:25 +0300

    baseEntity added with db configuration about the columns

--- src/main/java/com/accounting/entity/BaseEntity.java
+++ src/main/java/com/accounting/entity/BaseEntity.java
@@ -1,7 +1,34 @@
 package com.accounting.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    Long id;
+    @Column(nullable = false,updatable = false)
+    public LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
+    public Long insertUserId;
+    @Column(nullable = false)
+    public LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
+    public Long lastUpdateUserId;
+
+    private boolean isDeleted;
 }
