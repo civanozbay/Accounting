5c60fdb6a6b65171a517be0399b583c41f9b876e
civanozbay <civan417@hotmail.com>
2024-04-16 17:50:18 +0300

    invoice and invoiceProduct entities added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -81,6 +81,19 @@ values
 ('2022-09-15 00:00:00', 3, false, '2022-09-15 00:00:00', 3, 'Phone', 3),
 ('2022-09-15 00:00:00', 3, false, '2022-09-15 00:00:00', 3, 'TV', 3),
 ('2022-09-15 00:00:00', 3, false, '2022-09-15 00:00:00', 3, 'Monitor', 3);
+insert into clients_vendors(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+                            client_vendor_type, client_vendor_name, phone, website, address_id, company_id)
+values
+-- COMPANY-2 / Green Tech
+('2022-09-15T00:00',2, false,'2022-09-15T00:00',2,'CLIENT','Orange Tech', '+1 (251) 321-4155', 'https://www.orange.com', 5, 2),
+('2022-09-15T00:00',2, false,'2022-09-15T00:00',2,'CLIENT','Ower Tech', '+1 (251) 321-4141', 'https://www.ower.com', 8, 2),
+('2022-09-15T00:00',2, false,'2022-09-15T00:00',2,'VENDOR','Photobug Tech', '+1 (652) 852-3246', 'https://www.photobug.com', 6, 2),
+('2022-09-15T00:00',2, false,'2022-09-15T00:00',2,'VENDOR','Wordtune Tech','+1 (652) 852-3246','https://www.wordtune.com', 7, 2),
+-- COMPANY-3 / Blue Tech
+('2022-09-15T00:00',3, false,'2022-09-15T00:00',3,'CLIENT', 'Reallinks Tech', '+1 (652) 852-9544','https://www.reallinks.com', 3, 3),
+('2022-09-15T00:00',3, false,'2022-09-15T00:00',3,'VENDOR', 'Livetube Tech', '+1 (652) 852-2055','https://www.livetube.com', 4, 3),
+('2022-09-15T00:00',3, false,'2022-09-15T00:00',3,'CLIENT', 'Key Tech', '+1 (652) 852-7896','https://www.keytech.com', 1, 3),
+('2022-09-15T00:00',3, false,'2022-09-15T00:00',3,'VENDOR', 'Mod Tech', '+1 (652) 852-3648','https://www.modtech.com', 2, 3);
 
 insert into products(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
                      name, quantity_in_stock, low_limit_alert, product_unit, category_id)
@@ -93,3 +106,44 @@ VALUES
 -- COMPANY-3 / Blue Tech
 ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 'Samsung Galaxy S20 (renewed)', 10, 5, 'PCS', 3),
 ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 'Samsung Galaxy S22', 20, 5, 'PCS', 3);
+
+insert into invoices(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+                     date,invoice_no, invoice_type, invoice_status, client_vendor_id, company_id)
+values
+-- COMPANY-2 / Green Tech
+('2022-09-09 00:00', 2, 'false', '2022-09-09 00:00', 2, '2022-09-09', 'P-001', 'PURCHASE', 'APPROVED', 2, 2),
+('2022-09-10 00:00', 2, 'false', '2022-09-10 00:00', 2, '2022-09-10', 'P-002', 'PURCHASE', 'APPROVED', 3, 2),
+('2022-09-17 00:00', 2, 'false', '2022-09-17 00:00', 2, '2022-09-17', 'S-001', 'SALES', 'APPROVED', 1, 2),
+('2022-10-19 00:00', 2, 'false', '2022-10-19 00:00', 2, '2022-10-19', 'S-002', 'SALES', 'AWAITING_APPROVAL', 1, 2),
+('2022-11-20 00:00', 2, 'false', '2022-11-20 00:00', 2, '2022-11-20', 'S-003', 'SALES', 'AWAITING_APPROVAL', 1, 2);
+--
+-- -- COMPANY-3 / Blue Tech
+-- ('2022-09-09 00:00', 3, 'false', '2022-09-09 00:00', 3, '2022-09-09', 'P-001', 'PURCHASE', 'APPROVED', 5, 3),
+-- ('2022-09-10 00:00', 3, 'false', '2022-09-10 00:00', 3, '2022-09-10', 'P-002', 'PURCHASE', 'APPROVED', 5, 3),
+-- ('2022-09-13 00:00', 3, 'false', '2022-09-13 00:00', 3, '2022-09-13', 'S-001', 'SALES', 'APPROVED', 4, 3),
+-- ('2022-11-18 00:00', 3, 'false', '2022-11-18 00:00', 3, '2022-11-18', 'S-002', 'SALES', 'AWAITING_APPROVAL', 4, 3),
+-- ('2022-11-19 00:00', 3, 'false', '2022-11-19 00:00', 3, '2022-11-19', 'S-003', 'SALES', 'AWAITING_APPROVAL', 4, 3),
+-- ('2022-11-20 00:00', 3, 'false', '2022-11-20 00:00', 3, '2022-11-20', 'S-004', 'SALES', 'AWAITING_APPROVAL', 6, 3),
+-- ('2022-11-21 00:00', 3, 'false', '2022-11-21 00:00', 3, '2022-11-21', 'S-005', 'SALES', 'AWAITING_APPROVAL', 6, 3);
+--
+-- insert into invoice_products(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+--                              price,quantity, remaining_quantity, tax, profit_loss, invoice_id, product_id)
+-- values
+-- -- COMPANY-2 / Green Tech
+-- ('2022-09-15 00:00', 2, 'false', '2022-09-15 00:00', 2, 250, 5, 3, 10, 0, 1, 1),     --purchase APPROVED
+-- ('2022-09-15 00:00', 2, 'false', '2022-09-15 00:00', 2, 250, 5, 5, 10, 0, 2, 1),     --purchase APPROVED total cost (with tax) 2750
+-- ('2022-09-15 00:00', 2, 'false', '2022-09-15 00:00', 2, 300, 2, 0, 10, 110, 3, 1),    --sale APPROVED     total sale (with tax) 660 & profit : 110 with tax
+-- ('2022-09-15 00:00', 2, 'false', '2022-09-15 00:00', 2, 200, 2, 0, 10, 0, 4, 1),     --sale AWAITING_APPROVAL after approval total sale (with tax) :1100 & profit (with tax) : 0
+-- ('2022-09-15 00:00', 2, 'false', '2022-09-15 00:00', 2, 300, 5, 0, 10, 0, 5, 1),      --sale AWAITING_APPROVAL after approval total sale (with tax) :2750 & profit (with tax) : 275
+--
+-- -- COMPANY-3 / Blue Tech
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 200, 20, 0, 10, 0, 6, 5),     --purchase APPROVED
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 200, 20, 10, 10, 0, 6, 5),    --purchase APPROVED
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 900, 10, 10, 10, 0, 7, 6),    --purchase APPROVED
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 1000, 10, 10, 10, 0, 7, 6),  --purchase APPROVED  total cost (with tax) 29700
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 300, 10, 0, 10, 1100, 8, 5),  --sale APPROVED     total sale (with tax) 3300 & profit : 1100 with tax
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 300, 20, 0, 10, 2200, 8, 5),  --sale APPROVED     total sale (with tax): 9900 & total profit : 3300 with tax
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 1200, 6, 0, 10, 0, 9, 6),     --sale AWAITING_APPROVAL after approval total sale (with tax) : 17820 & profit (with tax) : 3300+1980=5280
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 1200, 5, 0, 10, 0, 10, 6),     --sale AWAITING_APPROVAL after approval total sale (with tax) : 24420 & profit (with tax) : 5280+1540=6820
+-- ('2022-09-15 00:00', 3, 'false', '2022-09-15 00:00', 3, 1200, 2, 0, 10, 0, 11, 6);    --sale AWAITING_APPROVAL after approval total sale (with tax) : 27060 & profit (with tax) : 6820+440=7260
+--
