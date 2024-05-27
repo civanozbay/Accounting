345f763ce1b78ce55ceee468248dc4ed8842260c
civanozbay <civan417@hotmail.com>
2024-05-27 15:34:23 +0200

    webconfig added

---
+++ src/main/java/com/accounting/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.accounting.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("login");
+    }
+
+}
